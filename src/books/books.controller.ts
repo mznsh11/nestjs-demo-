9255bad61d35debd2e084ee9f2afb5a423b325c5
mznsh11 <mazenshaaban58@gmial.com>
2026-05-22 16:50:32 +0300

    feat: implement books CRUD operations

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,6 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, Patch} from '@nestjs/common';
+
+
 import { BooksService } from './books.service';
 
 @Controller('books')
@@ -6,4 +8,31 @@ export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
   // TODO: Implement book controller methods
+  @Get()
+  findAll() {
+    return this.booksService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.booksService.findOne(+id);
+  }
+
+  @Post()
+  create(@Body() book: { title: string; authorId: number }) {
+    return this.booksService.create(book);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() bookUpdate: { title?: string; authorId?: number },
+  ) {
+    return this.booksService.update(+id, bookUpdate);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.booksService.delete(+id);
+  }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,11 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { AuthorsService } from '../authors/authors.service';
 
 @Injectable()
 export class BooksService {
   private books = [
-    // TODO: add some sample books here
+    {
+      id: 1,
+      title: 'Book1',
+      authorId: 1
+    },
+    {
+      id: 2,
+      title: 'Book2',
+      authorId: 2
+    },
+    {
+      id: 3,
+      title: 'Book3',
+      authorId: 3
+    }  
   ];
 
+
   constructor(private readonly authorsService: AuthorsService) {}
+
+  findAll() {
+    return this.books;
+  }
+
+  findOne(id: number) {
+    const book= this.books.find(book => book.id === id);
+    if (!book) {
+      throw new NotFoundException(`Book with id ${id} not found`);
+    }
+    return book;
+  }
+
+  create(book: { title: string; authorId: number }) {
+    const newBook = {
+      id: 
+        this.books.length > 0 ? this.books[this.books.length - 1].id + 1 : 1,
+      ...book,
+    };
+    this.books.push(newBook);
+    return newBook;
+  }
+
+  update(id: number, book: { title?: string; authorId?: number }) {
+    const bookIndex = this.books.findIndex(book => book.id === id);
+    if (bookIndex === -1) {
+      throw new NotFoundException(`Book with id ${id} not found`);
+    }
+    this.books[bookIndex] = {
+      ...this.books[bookIndex],
+      ...book,
+    };
+    return this.books[bookIndex];
+  }
+
+  delete(id: number) {
+    const bookIndex = this.books.findIndex(book => book.id === id);
+    if (bookIndex === -1) {
+      throw new NotFoundException(`Book with id ${id} not found`);
+    }
+    this.books.splice(bookIndex, 1);
+    return { message: `Book with id ${id} deleted successfully` };
+  }
 }
